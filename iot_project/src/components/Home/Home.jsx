3fcddad09cd45e4dfcac525156f98c18392c6b3e
Iker8av <iker8avv@gmail.com>
2022-11-16 14:16:55 -0600

    [Access] - Git Ignore Changes

---
+++ iot_project/src/components/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+// import Sheet from '../Sheet/Sheet'
+import Access from "./Views/Access/Access"
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <div className='Home'>
+        <header>
+            <div className='row'>
+                <h1>Welcome, Username!</h1> {/* User name prop */}
+            </div>
+            <Access/>
+        </header>
+        <section>
+          {/* Carrusel */}
+        </section>
+        {/* Sheet */}
+    </div>
+  )
+}
